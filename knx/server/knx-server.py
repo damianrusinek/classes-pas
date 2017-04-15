ce06d17a1944616227c29cba62893e983c88a41d
Damian Rusinek <damian.rusinek@gmail.com>
2017-04-15 21:27:13 +0200

    Added knx server

--- knx/server/knx-server.py
+++ knx/server/knx-server.py
@@ -12,12 +12,17 @@ KNX_STATUS_NAMES = {v: k for k, v in constants.KNX_STATUS_CODES.items()}
 DEFAULT_PORT = 3671
 EMPTY_CONNECTION = {
 	'address': None,
+	'sequence': 0,
 }
+
+GROUP_VALUE = 0
+MONITOR_PORT = 3333
 
 class KNXServer():
 
-	def __init__(self, port, options):
+	def __init__(self, group_address, port, options):
 		self.port = port
+		self.group_address = tuple(map(int, group_address.split('/')))
 		self.options = options
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.connections = {}
@@ -46,7 +51,10 @@ class KNXServer():
 		else:
 			m = msg.parse_message(data)
 		return m
-		
+	
+	def parse_group_address_integer(self, addr):
+			return addr >> 11 & 0x1f, addr >> 8 & 0x7, addr & 0xff
+
 	def create_new_communication_channel(self, client_addr):
 		status = KNX_STATUS_NAMES['E_NO_ERROR']
 		i = 0
@@ -64,6 +72,12 @@ class KNXServer():
 			self.connections[communication_channel] = EMPTY_CONNECTION
 			self.connections[communication_channel]['address'] = client_addr
 		return status, communication_channel
+
+	def get_next_sequence(self, communication_channel, current_sequence=None):
+		if current_sequence is not None:
+			self.connections[communication_channel]['sequence'] = current_sequence
+		self.connections[communication_channel]['sequence'] += 1
+		return self.connections[communication_channel]['sequence']
 		
 	def remove_communication_channel(self, communication_channel):
 		del self.connections[communication_channel]
@@ -76,20 +90,24 @@ class KNXServer():
 		logging.info('Received message from client %s: %s', client_addr, m.__class__.__name__)
 		
 		if isinstance(m,msg.KnxDescriptionRequest):
+			logging.debug('KnxDescriptionRequest')
 			resp = mmsg.KnxDescriptionResponse(request=m)
 			resp.pack_knx_message()
 			logging.debug("Sending KnxDescriptionResponse")
 			self.sock.sendto(resp.get_message(), client_addr)
 		
 		elif isinstance(m,msg.KnxConnectRequest):
+			logging.debug('KnxConnectRequest')
 			status, communication_channel = self.create_new_communication_channel(client_addr)
+			logging.debug("Preparing KnxConnectResponse")
 			resp = mmsg.KnxConnectResponse(request=m, communication_channel=communication_channel, status=status)
 			resp.pack_knx_message()
 			logging.debug("Sending KnxConnectResponse")
 			self.sock.sendto(resp.get_message(), client_addr)
 			
 		elif isinstance(m,msg.KnxDisconnectRequest):
-			communication_channel = m.body['communication_channel_id'] if m.body['communication_channel_id'] is not None else 0 
+			logging.debug('KnxDisconnectRequest')
+			communication_channel = m.body['communication_channel_id']
 			status = KNX_STATUS_NAMES['E_NO_ERROR'] if self.is_communication_channel_active(communication_channel) else KNX_STATUS_NAMES['E_CONNECTION_ID']
 			if self.is_communication_channel_active(communication_channel):
 				self.remove_communication_channel(communication_channel)
@@ -99,6 +117,7 @@ class KNXServer():
 			self.sock.sendto(resp.get_message(), client_addr)
 			
 		elif isinstance(m,msg.KnxConnectionStateRequest):
+			logging.debug('KnxConnectionStateRequest')
 			communication_channel = m.body['communication_channel_id']
 			status = KNX_STATUS_NAMES['E_NO_ERROR'] if self.is_communication_channel_active(communication_channel) else KNX_STATUS_NAMES['E_CONNECTION_ID']
 			resp = mmsg.KnxConnectionStateResponse(status=status, communication_channel=communication_channel, request=m)
@@ -106,8 +125,40 @@ class KNXServer():
 			logging.debug("Sending KnxConnectionStateResponse")
 			self.sock.sendto(resp.get_message(), client_addr)
 		
+		elif isinstance(m,msg.KnxTunnellingAck):
+			logging.debug('KnxTunnellingAck')
+		
 		elif isinstance(m,msg.KnxTunnellingRequest):
-			pass
+			logging.debug('KnxTunnellingRequest')
+			communication_channel = m.body['communication_channel_id']
+			if not self.is_communication_channel_active(communication_channel):
+				return
+
+			resp = mmsg.KnxTunnellingAck(request=m, communication_channel=communication_channel, sequence_count=m.body['sequence_counter'])
+			resp.pack_knx_message()
+			logging.debug("Sending KnxConnectionStateResponse")
+			self.sock.sendto(resp.get_message(), client_addr)
+
+			knx_destination = '/'.join(map(str, self.parse_group_address_integer(m.body['cemi']['knx_destination'])))
+
+			# Send confirmation
+			if m.body['cemi']['message_code'] == constants.CEMI_MSG_CODES['L_Data.req']:
+				resp = mmsg.KnxTunnellingRequestConfirmation(request=m, communication_channel=communication_channel, 
+					sequence_count=self.get_next_sequence(communication_channel,m.body['sequence_counter']), sockname=client_addr,
+					knx_source='0.0.0', knx_destination=knx_destination)
+				logging.debug("Sending KnxTunnellingRequestConfirmation")
+				self.sock.sendto(resp.get_message(), client_addr)
+
+			#Send indicator
+			if self.group_address == self.parse_group_address_integer(m.body['cemi']['knx_destination']):
+				global GROUP_VALUE
+				GROUP_VALUE = m.body['cemi']['apci']['data']
+				resp = mmsg.KnxTunnellingRequestIndicator(request=m, communication_channel=communication_channel, 
+					sequence_count=self.get_next_sequence(communication_channel,m.body['sequence_counter']), sockname=client_addr,
+					knx_source='1.1.1', knx_destination=knx_destination)
+				logging.debug("Sending KnxTunnellingRequestIndicator")
+				self.sock.sendto(resp.get_message(), client_addr)
+			
 		else:
 			logging.error('Unknown knx message.')
 			
@@ -121,27 +172,65 @@ class KNXServer():
 			t = threading.Thread(target=self.handle_request, args=(data, client_addr))
 			t.start()
 
+def group_value_monitor():
+	global GROUP_VALUE
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	addr = ('0.0.0.0', MONITOR_PORT)
+	sock.bind(addr)
+	logging.debug("Monitor started at %s:%d" % addr)
+  
+	while True:
+		data, client_addr = sock.recvfrom(1)
+		logging.debug("Received monitor request from %s:%d" % client_addr)
+		sock.sendto(bytes(map(ord, list(str(GROUP_VALUE)))), client_addr)
+
 def main():
-	usage = "usage: %prog [options] port"
+	usage = "usage: %prog [options] [port] group_address"
 	parser = OptionParser(usage=usage)
 	parser.add_option('-v', '--verbose', action='store_true', dest='verbose')
 	(options, args) = parser.parse_args()
-	if len(args) != 1:
-		port = DEFAULT_PORT
-	else:
+	if len(args) == 2:
 		try:
 			port = int(args[0])
 		except ValueError:
 			parser.error("port must be an integer")
+
+		if len(args[1].split('/')) != 3:
+			parser.error("invalid group_address format")
+		else:
+			for i in args[1].split('/'):
+				try:
+					if len(i) != 1:
+						raise ValueError()
+					port = int(i)
+				except ValueError:
+					parser.error("group_address can consist of integers only")
+		group_address = args[1]
+	elif len(args) == 1:
+		port = DEFAULT_PORT
+		if len(args[0].split('/')) != 3:
+			parser.error("invalid group_address format")
+		else:
+			for i in args[0].split('/'):
+				try:
+					if len(i) != 1:
+						raise ValueError()
+					int(i)
+				except ValueError:
+					parser.error("group_address can consist of integers only")
+		group_address = args[0]
+	else:
+		parser.error("group_address is required")
 	
 	if options.verbose:
 		logging.getLogger().setLevel(logging.DEBUG)
 	
-	server = KNXServer(port, options)
+	# Monitor
+	t = threading.Thread(target=group_value_monitor)
+	t.start()
+
+	server = KNXServer(group_address, port, options)
 	server.start()
-	
-	
-	
 
 if __name__ == '__main__':
 	main()
--- knx/server/messages.py
+++ knx/server/messages.py
@@ -12,7 +12,7 @@ class KnxDescriptionResponse(msg.KnxDescriptionResponse):
 		super(KnxDescriptionResponse, self).__init__(message=message)
 
 	def _pack_knx_body(self):
-		self.body = bytearray.fromhex('3601020000000000b8874bdf6e7fe000170c0242ac1100026b6e7864000000000000000'+
+		self.body = bytearray.fromhex('3601020000000000b8874bdf6e7fe000170c0242ac1100026b6e786420706173000000000000000'+
 		'00000000000000000000000000000000000000a020201030104010501')
 		return self.body
 
@@ -21,8 +21,8 @@ class KnxConnectResponse(msg.KnxConnectResponse):
 
 	def __init__(self, message=None, request=None, communication_channel=None, status=0):
 		self.request = request
-		self.status = status
 		self.communication_channel = communication_channel
+		self.status = status
 		super(KnxConnectResponse, self).__init__(message=message)
 
 	def _pack_knx_body(self):
@@ -34,6 +34,7 @@ class KnxDisconnectResponse(msg.KnxDisconnectResponse):
 
 	def __init__(self, message=None, request=None, communication_channel=1, status=0):
 		self.request = request
+		self.communication_channel = communication_channel
 		self.status = status
 		super(KnxDisconnectResponse, self).__init__(message=message, communication_channel=communication_channel)
 
@@ -47,8 +48,46 @@ class KnxConnectionStateResponse(msg.KnxConnectionStateResponse):
 	def __init__(self, message=None, request=None, communication_channel=1, status=0):
 		self.request = request
 		self.status = status
+		self.communication_channel = communication_channel
 		super(KnxConnectionStateResponse, self).__init__(message=message, communication_channel=communication_channel)
 
 	def _pack_knx_body(self):
 		self.body = bytearray.fromhex(('%02x' % self.communication_channel) + ('%02x' % self.status))
+		return self.body
+
+
+class KnxTunnellingAck(msg.KnxTunnellingAck):
+
+	def __init__(self, request = 0, message=None, communication_channel=None, sequence_count=0):
+		self.request = request
+		super(KnxTunnellingAck, self).__init__(message=message, communication_channel=communication_channel, sequence_count=sequence_count)
+
+
+class KnxTunnellingRequestConfirmation(msg.KnxTunnellingRequest):
+
+	def __init__(self, request = 0,  message=None, sockname=None, communication_channel=None,
+                 knx_source=None, knx_destination=None, sequence_count=0):
+		self.request = request
+		super(KnxTunnellingRequestConfirmation, self).__init__(message=message, communication_channel=communication_channel, 
+			sequence_count=sequence_count,sockname=sockname, knx_source=knx_source, knx_destination=knx_destination, 
+			message_code=0x2e, cemi_ndpu_len=0)
+
+	def _pack_knx_body(self, cemi=None):
+		self.body = super(KnxTunnellingRequestConfirmation, self)._pack_knx_body(cemi=cemi) + bytes([0x01,0x00,0x41])
+		self.body = self.body[:6] + bytes([0xbc, 0xd0]) + self.body[8:]
+		return self.body
+
+
+class KnxTunnellingRequestIndicator(msg.KnxTunnellingRequest):
+
+	def __init__(self, request = 0,  message=None, sockname=None, communication_channel=None,
+                 knx_source=None, knx_destination=None, sequence_count=0):
+		self.request = request
+		super(KnxTunnellingRequestIndicator, self).__init__(message=message, communication_channel=communication_channel, 
+			sequence_count=sequence_count,sockname=sockname, knx_source=knx_source, knx_destination=knx_destination, 
+			message_code=0x29, cemi_ndpu_len=0)
+
+	def _pack_knx_body(self, cemi=None):
+		self.body = super(KnxTunnellingRequestIndicator, self)._pack_knx_body(cemi=cemi) + bytes([0x01,0x00,0x81])
+		self.body = self.body[:6] + bytes([0xbc, 0xc0]) + self.body[8:]
 		return self.body

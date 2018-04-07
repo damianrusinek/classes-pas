63aa94c9365c56d72542f87d76d8f06729b995ee
Emilian Lato <Emilian.Lato@Live.umcs.edu.pl>
2018-04-07 13:54:19 +0200

    Add dns client in dotnet and readme for setting cross platform .net project

---
+++ dns_client/dns_client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using static System.Net.Dns;
+
+namespace DnsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // c# 7.0 tinkering, use this version of input error handling if you want
+            //string hostname = args.Length == 1
+            //    ? args[0]
+            //    : throw new ArgumentException("Usage: run with parameter <host-name>");
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Usage: run with parameter <host-name>");
+                Environment.Exit(1);
+            }
+            var hostname = args[0];
+            try
+            {
+                IPHostEntry entry = GetHostEntry(hostname);
+                //GetHostByName method is obsolete
+                var ip = entry.AddressList.FirstOrDefault();
+                Console.WriteLine($"Resolved IP address: {ip}");
+#if DEBUG
+                Console.Read();
+#endif
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+        }
+    }
+}
